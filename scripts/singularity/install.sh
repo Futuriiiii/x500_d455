ec4b40f1067c149af707bb306cd891a03000b604
Futuriiiii <rafael.novack07@gmail.com>
2023-08-09 18:05:45 -0300

    update singularity script

--- scripts/singularity/install.sh
+++ scripts/singularity/install.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 # get the path to this script
-echo pwd
-MY_PATH=/../../home/rafa/mrs_singularity/user_ros_workspace/src/x500_d455
+MY_PATH=/home/rafa/mrs_singularity/user_ros_workspace/src/x500_d455
 
 simulation_path=/opt/mrs/mrs_workspace/install/share/mrs_simulation
 
 # Ask about udev rules
 while true; do
 
-	cp -fsT "$MY_PATH"/models/meshes "$simulation_path"/models/mrs_robots_description/meshes/custom/d455
+	ln -fsT "$MY_PATH"/models/meshes "$simulation_path"/models/mrs_robots_description/meshes/custom/d455
 
 	# add suspended load option to the spawner configs
 	file="$simulation_path"/config/spawner_params.yaml
@@ -26,12 +25,12 @@ while true; do
 	  # add d455
 	  if ! grep -q "$j"\_d455 "$file"; then
 		sed -i '/\/model/ i \\t{%- include "mrs_robots_description/sdf/'$j'_d455.sdf.jinja" -%}' "$file"
-		cp -fs "$MY_PATH"/models/sdf/"$j"\_d455.sdf.jinja "$simulation_path"/models/mrs_robots_description/sdf/.
+		ln -fs "$MY_PATH"/models/sdf/"$j"\_d455.sdf.jinja "$simulation_path"/models/mrs_robots_description/sdf/.
 		echo $'\e[1;32mAdded d455 file.\e[0m'
 	  fi
 	  # add macro file for d455
 	  if ! grep -q d455_macro "$file"; then
-		cp -fs "$MY_PATH"/models/sdf/d455_macro.sdf.jinja "$simulation_path"/models/mrs_robots_description/sdf/.
+		ln -fs "$MY_PATH"/models/sdf/d455_macro.sdf.jinja "$simulation_path"/models/mrs_robots_description/sdf/.
 		echo $'\e[1;32mAdded d455 macro file.\e[0m\n'
 	  fi
 	done
